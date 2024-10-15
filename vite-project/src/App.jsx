144db6ad40d9dff8268a4a776824584232adfb38
chandjamba <chandjamba2001@gmail.com>
2024-10-15 11:42:43 -0400

    working on styling the last component

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import Balance from "./Components/Balance";
+import History from "./Components/History";
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <Balance />
+      <History />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite-project/src/Components/History.jsx
@@ -0,0 +1,33 @@
+import "./history.scss";
+
+export default function History() {
+  return (
+    <div className="history">
+      <div className="history__inner-container">
+        <div className="history__heading-box">History</div>
+        <div className="history__slots-wrapper">
+          <div className="history__slot-1">
+            <div className="history__slot-item-box">Grocery</div>
+            <div className="history__slot-item-price-box">-300</div>
+          </div>
+          <div className="history__slot-1">
+            <div className="history__slot-item-box">Monitor</div>
+            <div className="history__slot-item-price-box">-1000</div>
+          </div>
+          <div className="history__slot-1">
+            <div className="history__slot-item-box">Clothing</div>
+            <div className="history__slot-item-price-box">-2000</div>
+          </div>
+          <div className="history__slot-1">
+            <div className="history__slot-item-box">Utilities</div>
+            <div className="history__slot-item-price-box">-200</div>
+          </div>
+          <div className="history__slot-1">
+            <div className="history__slot-item-box">Rent</div>
+            <div className="history__slot-item-price-box">-600</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
